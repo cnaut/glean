c8d7862cabeaeb18e0e787b59669385f5f1512fa
cnaut <cpn585@gmail.com>
2013-08-19 03:13:56 +0000

    Pull my body fat data from fitbit for a particular date range using the temboo sdk and display it on the screen

--- web.js
+++ web.js
@@ -1,14 +1,25 @@
 var express = require('express')
 var fs = require('fs');
-var htmlfile = "index.html";
+var tsession = require("temboo/core/temboosession");
+var session = new tsession.TembooSession("glean", process.env.TEMBOO_APP_NAME, process.env.TEMBOO_APP_KEY);
+var Fitbit = require("temboo/Library/Fitbit/Body");
 
 var app = express();
 app.get('/', function(request, response) {
-	var html = fs.readFileSync(htmlfile).toString();
-	response.send(html);
+    var getBodyFatChoreo = new Fitbit.GetBodyFat(session); 
+    var getBodyFatInputs = getBodyFatChoreo.newInputSet();
+
+    getBodyFatInputs.setCredential('Fitbit');
+    getBodyFatInputs.set_Date("2013-07-01/2013-07-14");
+
+    getBodyFatChoreo.execute(
+	getBodyFatInputs, 
+	function(results) { response.send(results.get_Response()); },
+	function(error) { console.log(error.message); }
+    );
 });
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() { 
-	console.log("Listening on " + port);
+    console.log("Listening on " + port);
 }); 
