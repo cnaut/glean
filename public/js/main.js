2cc51bfe41b3b50c6c4d59ca20da0340168e6ab2
cnaut <cpn585@gmail.com>
2013-08-29 12:37:25 +0000

    Change bar chart to from horizontal to vertical bar chart

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,6 @@ function generateChart() {
     chart.selectAll("div")
 	.data(fatPercents)
     .enter().append("div")
-	.style("width", function(d) { return d * 10 + "px"; })
+	.style("height", function(d) { return d * 10 + "px"; })
 	.text(function(d) { return d; });
 }
