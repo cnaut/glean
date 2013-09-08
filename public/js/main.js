e882760edc0a1a8e4aef17c984924a0098566866
cnaut <cpn585@gmail.com>
2013-09-08 04:14:23 +0000

    Clean up client side javascript code that generates chart

--- public/js/main.js
+++ public/js/main.js
@@ -1,71 +1,22 @@
-function generateChart() {
-    var fatPercents = JSON.parse(document.body.getAttribute("fatPercents"));
-    var weekLow = JSON.parse(document.body.getAttribute("weekLow"));
-    var weekHigh = JSON.parse(document.body.getAttribute("weekHigh"));
+function generateCharts() {
+    var fatPercents = getData("fatPercents");
+    var weekLow = getData("weekLow");
+    var weekHigh = getData("weekHigh");
 
-    var predictedData = JSON.parse(document.body.getAttribute("predictedData"));
-    var fatGoal = JSON.parse(document.body.getAttribute("fatGoal"));
+    var predictedData = getData("predictedData");
+    var fatGoal = getData("fatGoal");
     
-    var x1 = JSON.parse(document.body.getAttribute("x1"));
-    var y1 = JSON.parse(document.body.getAttribute("y1"));
-    var x2 = JSON.parse(document.body.getAttribute("x2"));
-    var y2 = JSON.parse(document.body.getAttribute("y2"));
+    var x1 = getData("x1");
+    var y1 = getData("y1");
+    var x2 = getData("x2");
+    var y2 = getData("y2");
 
-    var highChart = d3.select("#highest-chart-div").append("div")
-	.attr("class", "chart")
-	.attr("id", "highest-chart");
+    generateChart(weekHigh, "#highest-chart-div", "highest-chart");    
+    generateChart(weekLow, "#lowest-chart-div", "lowest-chart");    
+    generateChart(fatPercents, "#historical-chart-div", "historical-chart");    
+    generateChart(predictedData, "#predicted-chart-div", "predicted-chart");    
+    generateChart(fatGoal, "#goal-chart-div", "goal-chart");
     
-    highChart.selectAll("div")
-	.data(weekHigh)
-    .enter().append("div")
-	.style("height", function(d) { return d * 10 + "px"; })
-	.style("width", "30px")
-	.text(function(d) { return d; });
-
-    var lowChart = d3.select("#lowest-chart-div").append("div")
-	.attr("class", "chart")
-	.attr("id", "lowest-chart");
-    
-    lowChart.selectAll("div")
-	.data(weekLow)
-    .enter().append("div")
-	.style("height", function(d) { return d * 10 + "px"; })
-	.style("width", "30px")
-	.text(function(d) { return d; });
-
-    var chart1 = d3.select("#historical-chart-div").append("div")
-	.attr("class", "chart")
-	.attr("id", "historical-chart");
-    
-    chart1.selectAll("div")
-	.data(fatPercents)
-    .enter().append("div")
-	.style("height", function(d) { return d * 10 + "px"; })
-	.style("width", "30px")
-	.text(function(d) { return d; });
-    
-    var chart2 = d3.select("#predicted-chart-div").append("div")
-	.attr("class", "chart")
-	.attr("id", "predicted-chart");
-    
-    chart2.selectAll("div")
-	.data(predictedData)
-    .enter().append("div")
-	.style("height", function(d) { return d * 10 + "px"; })
-	.style("width", "30px")
-	.text(function(d) { return d; });
-    
-    var chart3 = d3.select("#goal-chart-div").append("div")
-	.attr("class", "chart")
-	.attr("id", "goal-chart");
-    
-    chart3.selectAll("div")
-	.data(fatGoal)
-    .enter().append("div")
-	.style("height", function(d) { return d * 10 + "px"; })
-	.style("width", "30px")
-	.text(function(d) { return d; });
-
     var lineGraph = d3.select("#line-graph")
 	.append("svg:svg"); 
 
@@ -76,4 +27,21 @@ function generateChart() {
 	.attr("y2", y2 - 10)
 	.style("stroke", "red")
 	.style("stroke-width", 2);
+}
+
+function generateChart(data, parentName, elementName) {
+    var chart = d3.select(parentName).append("div")
+	.attr("class", "chart")
+	.attr("id", elementName);
+    
+    chart.selectAll("div")
+	.data(data)
+    .enter().append("div")
+	.style("height", function(d) { return d * 10 + "px"; })
+	.style("width", "30px")
+	.text(function(d) { return d; });
+}
+
+function getData(name) {
+    return JSON.parse(document.body.getAttribute(name));
 }
